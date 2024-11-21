a1e33b4b746c3d9e16469cee34b75ba8eefa5e70
Filipa Marta <filipa.marta@holisticon.de>
2024-11-21 17:31:56 +0000

    footer component

---
+++ app/components/Footer.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import styles from "./footer.module.css";
+import Image from "next/image";
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.footerInnerContainer}>
+        <h3>
+          Say hey
+          <br />
+          <span>filipa.marta@gmail.com</span>
+        </h3>
+        <ul>
+          <li>
+            {/* <Image src="" alt="Social media" width={40} height={40} /> */}
+          </li>
+          <li>
+            {/* <Image src="" alt="Social media" width={40} height={40} /> */}
+          </li>
+          <li>
+            {/* <Image src="" alt="Social media" width={40} height={40} /> */}
+          </li>
+          <li>
+            {/* <Image src="" alt="Social media" width={40} height={40} /> */}
+          </li>
+          <li>
+            {/* <Image src="" alt="Social media" width={40} height={40} /> */}
+          </li>
+        </ul>
+        <p>
+          Copyright 2024 made with lot&apos;s of love and magic by me, Filipa
+        </p>
+      </div>
+    </footer>
+  );
+}
