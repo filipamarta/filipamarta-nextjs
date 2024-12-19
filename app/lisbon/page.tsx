6464104900a0a4a2cbc8c0b6a45457d45a2344e7
Filipa Marta <filipa.marta@gmail.com>
2024-12-19 14:42:17 +0000

    update Lisbon page

--- app/lisbon/page.tsx
+++ app/lisbon/page.tsx
@@ -1,40 +1,109 @@
 "use client";
-import Header from "../components/Header";
+import Header from "../components/header/Header";
 import styles from "./page.module.css";
-import Cursor from "../components/Cursor";
-import Footer from "../components/Footer";
+import Cursor from "../components/cursor/Cursor";
+import Footer from "../components/footer/Footer";
 import parse from "html-react-parser";
 import Link from "next/link";
+import Image from "next/image";
+import { useRef } from "react";
+import { motion, MotionValue, useScroll, useTransform } from "framer-motion";
+import { useMediaQuery } from "react-responsive";
 
 const content = {
-  title: "Lisbon is the city where I was born.",
+  title: "Lisbon, the city where I was born",
   paragraphs: [
-    "A while ago I made a list of the things I love about the city and whenever I happen to be in conversation and know that friends and acquaintances are coming to visit, I share it with them.",
-    "And why not share it with you, people and bots who appear in this corner of the internet?",
-    "Let's do it, happy adventures in the nooks and crannies of Lisbon.",
+    "Some time ago, I compiled a list of things I love about the city. Whenever I find myself chatting with friends or acquaintances who are planning a visit, I like to share it with them.",
+    "And why not share it here, with all of you — people and bots alike — who wander through this corner of the internet?",
+    "So, here it is: Happy adventures in the hidden gems of Lisbon!",
   ],
 };
 
+function useParallax(value: MotionValue<number>, distance: number) {
+  return useTransform(value, [0, 1], [-distance, distance]);
+}
+
 export default function Page() {
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({ target: ref });
+  const y = useParallax(scrollYProgress, 300);
+  const isDesktopOrLaptop = useMediaQuery({ minWidth: 900 });
+
+  const left = useTransform(
+    scrollYProgress,
+    [0, 1],
+    ["calc(0vw + (0px))", "calc(100vw + -230px)"]
+  );
+  console.log(isDesktopOrLaptop);
+
   return (
     <>
       <div className={styles.page}>
         <Header />
-        <main className={styles.main}>
+        <main className={styles.main} ref={ref}>
+          <motion.section
+            className={styles.hero}
+            style={{ position: "relative", left, x: -200, bottom: -y }}
+          >
+            <Image
+              src="/backgroundImg-lisbon2.jpg"
+              alt="Lisbon"
+              width={1200}
+              height={769}
+              quality={100}
+              priority
+              className={styles.backgroundImage}
+            />
+          </motion.section>
           <section className={styles.textContainer}>
-            <h1>{parse(content.title)}</h1>
+            <motion.h1
+              initial={{ y: 50, opacity: 0, scale: 1.2 }}
+              whileInView={{
+                y: isDesktopOrLaptop ? -100 : 0,
+                opacity: 1,
+                scale: 1,
+                transition: { duration: 0.8 },
+              }}
+              viewport={{ once: true }}
+            >
+              {parse(content.title)}
+            </motion.h1>
             {content.paragraphs.map((paragraph, i) => {
-              return <p key={i}>{parse(paragraph)}</p>;
+              return (
+                <motion.p
+                  initial={{ x: -50, opacity: 0 }}
+                  whileInView={{
+                    x: 0,
+                    opacity: 1,
+                    transition: { duration: 1, delay: 0.3 * i },
+                  }}
+                  viewport={{ amount: "10%" }}
+                  key={i}
+                >
+                  {parse(paragraph)}
+                </motion.p>
+              );
             })}
 
-            <Link
-              href="https://maps.app.goo.gl/JDpATAny3te2cbnQ6"
-              passHref={true}
-              className="button-type"
-              target="_blank"
+            <motion.div
+              className={styles.buttonContainer}
+              initial={{ y: -100, opacity: 0 }}
+              whileInView={{
+                y: 0,
+                opacity: 1,
+                transition: { duration: 1, delay: 0.5 },
+              }}
+              viewport={{ amount: "10%" }}
             >
-              Lisbon (Google Map)
-            </Link>
+              <Link
+                href="https://maps.app.goo.gl/JDpATAny3te2cbnQ6"
+                passHref={true}
+                className="button-type"
+                target="_blank"
+              >
+                Lisbon (Google Map)
+              </Link>
+            </motion.div>
           </section>
         </main>
         <Footer />
